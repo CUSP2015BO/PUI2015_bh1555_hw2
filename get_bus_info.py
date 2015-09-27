869841a7d18fba040e209637dbb065db72e6b3bc
CUSP2015BO <bh1555@nyu.edu>
2015-09-26 21:53:36 -0400

    Update get_bus_info.py

--- get_bus_info.py
+++ get_bus_info.py
@@ -29,18 +29,4 @@ if __name__=='__main__':
 
 
     
-       
-    # Count the number of active buses
-    #NUM_BUS = len(response['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity'])
 
-    # print bus line and the number of active buses
-    #print "Bus Line : %s" % (BUS_LINE)
-    #print "Number of Active Buses : %s"  % (NUM_BUS)
-    
-    # print location of each active bus by using for loop 
-    #i=0
-    #for vehicle in response['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']:
-    #    Lat = vehicle['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
-    #    Lon = vehicle['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
-    #    i+=1
-    #    print " * Bus %s is at latitude %s and longitude %s" % (i, Lat, Lon)
